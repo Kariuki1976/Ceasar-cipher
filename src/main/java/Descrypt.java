61c54108a89fe0de2adddbfea056d283521e8e68
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-05-04 13:04:06 +0300

    create descrpt function

---
+++ src/main/java/Descrypt.java
@@ -0,0 +1,61 @@
+public class Descrypt {
+    private final String mInputString;
+    private int mShift;
+
+    public Descrypt(String mInputString, int mShift) {
+        this.mInputString = mInputString;
+        this.mShift = mShift;
+    }
+
+    public Descrypt(String mInputString) {
+        this.mInputString = mInputString;
+    }
+
+    public String getmInputString(){
+        return mInputString;
+    }
+
+    public int getmShift(){
+        return mShift;
+    }
+
+    public static String decrypt(Descrypt decrypt){
+
+        if(decrypt.mShift > 26){
+            decrypt.mShift = decrypt.mShift % 26;
+        }
+        else if(decrypt.mShift < 0) {
+            decrypt.mShift = (decrypt.mShift % 26) + 26;
+        }
+
+        StringBuilder outputString = new StringBuilder();
+        int length = decrypt.mInputString.length();
+        for (int i = 0; i < length ; i++) {
+            char cc = decrypt.mInputString.charAt(i);
+            if(Character.isLetter(cc)){
+                if(Character.isLowerCase(cc)){
+                    char c = (char)(cc-decrypt.mShift);
+                    if(c<'a'){
+                        outputString.append((char) (cc + (26 - decrypt.mShift)));
+                    }
+                    else {
+                        outputString.append(c);
+                    }
+                }
+                else if(Character.isUpperCase(cc)){
+                    char c = (char)(cc-decrypt.mShift);
+                    if(c<'A'){
+                        outputString.append((char) (cc + (26 - decrypt.mShift)));
+                    }
+                    else {
+                        outputString.append(c);
+                    }
+                }
+            }
+            else{
+                outputString.append(cc);
+            }
+        }
+        return outputString.toString();
+    }
+}
