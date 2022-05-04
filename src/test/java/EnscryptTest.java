ccb79be8a97a391f154c7a185931162bcad1a90f
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-05-04 13:35:33 +0300

    add enscryptTest.java class to test directory

--- src/test/java/EnscryptTest.java
+++ src/test/java/EnscryptTest.java
@@ -1,2 +1,30 @@
-public class EnscryptTest {
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncryptTest {
+    @Test
+    public void runEncrypt_instantiates(){
+        Enscrypt testEncrypt = new Enscrypt("abc",2);
+        assertEquals(true, testEncrypt instanceof Enscrypt);
+    }
+
+    @Test
+    public void runEncrypt_ifInputIsString() {
+        Enscrypt testEncrypt = new Enscrypt("abc",3);
+        assertEquals("abc", testEncrypt.getmInputString());
+    }
+
+    @Test
+    public void runDecrypt_ifKeyIsInt() {
+        Enscrypt testEncrypt = new Enscrypt("abc",2);
+        assertEquals(2, testEncrypt.getmShift());
+    }
+    @Test
+    public void runEncrypt_ifEncryptsVariousInputs() {
+        Enscrypt testEncrypt = new Enscrypt("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 3);
+        assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",Enscrypt.encrypt(testEncrypt));
+    }
 }
